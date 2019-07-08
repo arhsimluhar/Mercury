2f8f2275e2ee1c17358cc228df740b3acc7dbd29
rahul1809 <rahul.mishra2003@gmail.com>
2019-07-08 22:24:42 +0530

    added generic class based views.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
+
 # Create your models here.
 from django.utils import timezone
 
--- blog/urls.py
+++ blog/urls.py
@@ -9,8 +9,6 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     # post views
-    path('', views.post_list, name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
-         views.post_detail,
-         name='post_detail'),
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
 ]
