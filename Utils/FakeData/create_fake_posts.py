69558d3a5b1bde6e55a2f6661e0fffbcc63e4f14
rahul1809 <rahul.mishra2003@gmail.com>
2019-07-08 21:33:54 +0530

    fake post generation logic

--- Utils/FakeData/create_fake_posts.py
+++ Utils/FakeData/create_fake_posts.py
@@ -1,16 +1,21 @@
-import sys
+import os
 
-from django.contrib.auth.models import User
 from faker import Faker
 
-sys.path.append("../../blog")
+os.chdir("../../")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mercury.settings')
+import django
+
+django.setup()
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 from blog import models
 
-PostNum = 100
+PostNum = 20
 
 fake = Faker()
 user = User.objects.get(username='rahul')
 for i in range(PostNum):
-    print("coming here")
-    post = models.Post(title=fake.name_male(), author=user, body=fake.text())
+    title = fake.name_male()
+    post = models.Post(title=title, author=user, body=fake.text(), slug=slugify(title), status='published')
     post.save()
