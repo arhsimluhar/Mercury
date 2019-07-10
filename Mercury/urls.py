8042ee9f55f169b268e51c31f46f98e336c3a4cf
rahul1809 <rahul.mishra2003@gmail.com>
2019-07-10 14:41:56 +0530

    - added bootstrap and adding navbar to base.html - added new view for homepage

--- Mercury/urls.py
+++ Mercury/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
+    path('', views.homepage)
 ]
