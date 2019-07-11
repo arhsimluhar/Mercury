2c9ad10caf8c6775f7463c73888441059d394313
rahul1809 <rahul.mishra2003@gmail.com>
2019-07-11 21:45:03 +0530

    initialised seeker App.

--- blog/views.py
+++ blog/views.py
@@ -2,16 +2,26 @@
 
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+from taggit.models import Tag
 
 from .models import Post
 
 
 class PostListView(ListView):
-    queryset = Post.published.all()
+
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
     ordering = ["publish"]
+
+    def get_queryset(self):
+        if self.kwargs['tag_slug']:
+            tag = get_object_or_404(Tag, slug=self.kwargs['tag_slug'])
+            return Post.objects.all().filter(tags__in=[tag])
+        else:
+            return Post.objects.all()
+
+
 
 
 def post_detail(request, year, month, day, post):
