3b28ad13d048eecef6e69778462150aeb2b22ae8
rahul1809 <rahul.mishra2003@gmail.com>
2019-07-22 23:59:46 +0530

    adding rest framework requirements

--- Mercury/settings.py
+++ Mercury/settings.py
@@ -35,8 +35,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "blog",
     "taggit",
+    "pipeline",
+    "seeker",
+
 ]
 
 MIDDLEWARE = [
