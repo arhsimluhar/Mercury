d31e0136788af5908b143df970c62790f369aaaf
rahul1809 <rahul.mishra2003@gmail.com>
2019-08-01 23:50:17 +0530

    adding missingno visualisation and correcting syntax error

--- pipeline/visual/visual.py
+++ pipeline/visual/visual.py
@@ -1,10 +1,14 @@
+import missingno as msno
 import seaborn as sn
 
 
-class Draw(object):
-    def __int__(self, df=None):
-        if df:
+class Draw:
+    def __init__(self, df=None):
+        if len(df):
             self.df = df
 
     def heatmap(self):
         return sn.heatmap(self.df, annot=True)
+
+    def missing_data_visualisation(self):
+        return msno.matrix(self.df)
