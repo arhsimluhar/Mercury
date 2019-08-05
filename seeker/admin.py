60276ad57693d4962fde019efc2926c7085ea100
rahul1809 <rahul.mishra2003@gmail.com>
2019-08-05 19:04:47 +0530

    fixing the syntax error and adding logic to indentify variable types

--- seeker/admin.py
+++ seeker/admin.py
@@ -1,1 +1,12 @@
 # Register your models here.
+from django.contrib import admin
+
+# Register your models here.
+from .models import Solution
+
+
+@admin.register(Solution)
+class SolutionAdmin(admin.ModelAdmin):
+    list_display = ("id",)
+    date_hierarchy = "created"
+    ordering = ("-created",)
--- seeker/models.py
+++ seeker/models.py
@@ -1,1 +1,12 @@
 # Create your models here.
+from django.db import models
+
+
+class Solution(models.Model):
+    supported_languages = (("python2", "Python 2"), ("python3", "Python 3"), ("c++", "C++ 17"))
+    created = models.DateField(auto_now_add=True)
+
+    language = models.CharField(max_length=10, choices=supported_languages, default="c++")
+
+    class Meta:
+        ordering = ('-created',)
